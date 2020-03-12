57cc250b2bfebcbbc339bfa5a2353d2aafe4dd4b
rjcrter11 <rjcrter11@gmail.com>
2020-03-12 11:49:18 -0500

    adds start script

--- api/api-router.js
+++ api/api-router.js
@@ -1,29 +1,31 @@
-const express = require('express');
+const express = require("express");
 
-const Shouts = require('../shouts/shouts-model.js');
+const Shouts = require("../shouts/shouts-model.js");
 
 const router = express.Router();
 
 router.use(express.json());
 
-router.get('/', (req, res) => {
-  res.status(200).json({ api: 'up' });
+router.get("/", (req, res) => {
+  const environment = process.env;
+  const port = process.env.PORT || 5000;
+  res.status(200).json({ api: "up", port, environment });
 });
 
-router.get('/shouts', (req, res, next) => {
+router.get("/shouts", (req, res, next) => {
   Shouts.find()
-    .then(shouts => {
+    .then((shouts) => {
       res.status(200).json(shouts);
     })
-    .catch(error => next(error));
+    .catch((error) => next(error));
 });
 
-router.post('/shouts', (req, res, next) => {
+router.post("/shouts", (req, res, next) => {
   Shouts.add(req.body)
-    .then(shout => {
+    .then((shout) => {
       res.status(201).json(shout);
     })
-    .catch(error => next(error));
+    .catch((error) => next(error));
 });
 
 router.use(errorHandler);
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const server = require('./api/server.js');
+const server = require("./api/server.js");
 
-const port = 4000;
+// make the port be assigned by the Server
+// heroku will plact the .PORT environment value on their server
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
